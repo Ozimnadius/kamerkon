89a5f2f6f4ec0af3f8973f5f59be27231546f7f7
Mikle Seregin <mikhail-seregin@yandex.ru>
2022-10-20 11:46:23 +0300

    update

--- js/custom.js
+++ js/custom.js
@@ -5,3 +5,5 @@ window.addEventListener('load', function (){
        
     }());
 });
+
+// javascript:(function () { var script = document.createElement('script'); script.src="//cdn.jsdelivr.net/npm/eruda"; document.body.appendChild(script); script.onload = function () { eruda.init() } })();
--- src/components/main/script.js
+++ src/components/main/script.js
@@ -1,28 +1,25 @@
 window.addEventListener('load', function () {
-    let main = document.querySelector('.main');
 
-    if (!window.matchMedia('(max-width: 767.98px)').matches) {
+    let mainObj = document.querySelector('.main');
 
-        if (main) {
+    if (mainObj) {
 
-            setTimeout(function () {
-                document.querySelector('.main__title').classList.add('animate');
-            }, 100);
+        setTimeout(function () {
+            document.querySelector('.main__title').classList.add('animate');
+        }, 100);
 
-            let tlVideos = gsap.timeline();
-            tlVideos.to('.main-videos__item--1', {opacity: 0, duration: 1}, 3.5);
-            tlVideos.to('.main-videos__item--2', {opacity: 1, duration: 1.8}, 3.6);
-            tlVideos.to('.main-videos__flash', {scale: 2, duration: 2}, 3.6)
-            tlVideos.to('.main-videos__flash', {scale: 0, duration: 1}, 5.3);
+        let tlVideos = gsap.timeline();
+        tlVideos.to('.main-videos__item--1', {opacity: 0, duration: 1}, 3.5);
+        tlVideos.to('.main-videos__item--2', {opacity: 1, duration: 1.8}, 3.6);
+        tlVideos.to('.main-videos__flash', {scale: 2, duration: 2}, 3.6)
+        tlVideos.to('.main-videos__flash', {scale: 0, duration: 1}, 5.3);
 
 
-            setTimeout(function () {
-                document.querySelector('.header').classList.add('animate');
-                document.querySelector('.main-menu').classList.add('animate');
-            }, 3600);
-
-        }
+        setTimeout(function () {
+            document.querySelector('.header').classList.add('animate');
+            document.querySelector('.main-menu').classList.add('animate');
+        }, 3600);
 
     }
-
 });
+
